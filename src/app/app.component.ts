a3bc92eb790b664c62cc0b14dc5d6e8dbbda57d9
AndresUV7 <andres.ullauri18@gmail.com>
2020-02-19 19:23:44 -0500

    Test orientacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    console.log("object");
-    screen.orientation.lock('portrait');
+
   }
   title = 'ar-kids-pwa';
 }
