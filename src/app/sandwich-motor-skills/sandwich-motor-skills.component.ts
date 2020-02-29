2fac8667c137d9c1717c14faa2e69190ca6d6097
AndresUV7 <andres.ullauri18@gmail.com>
2020-02-28 21:45:20 -0500

    Test multi touch

--- src/app/sandwich-motor-skills/sandwich-motor-skills.component.ts
+++ src/app/sandwich-motor-skills/sandwich-motor-skills.component.ts
@@ -16,6 +16,7 @@ class NewScene extends Phaser.Scene {
   preload() {
  
     this.load.path = '/ar-kids-pwa/assets/img/'
+    // this.load.path = '/assets/img/'
 
     this.load.image("angular1","angular.png");
     this.load.image("angular2","angular.png");
@@ -28,8 +29,8 @@ class NewScene extends Phaser.Scene {
     this.input.addPointer(1);
     this.angular1=this.add.image(210,65,"angular1").setInteractive();
     this.input.setDraggable(this.angular1);
-    ;
-  
+    
+    console.log(this.angular1.x);     
 
     const eventos = Phaser.Input.Events;
 
@@ -42,25 +43,31 @@ class NewScene extends Phaser.Scene {
     })
 
 
-    this.graphics = this.add.graphics();
+    // this.graphics = this.add.graphics();
 
-    this.add.text(10, 10, 'Multi touch drag test', { font: '16px Courier', fill: '#000000' });
+    // this.add.text(10, 10, 'Multi touch drag test', { font: '16px Courier', fill: '#000000' });
 
     }
 
   update(time, delta){
 
+    if(this.input.pointer1.x>210){
 
-    if (this.input.pointer1.isDown || this.input.pointer2.isDown)
-    {
-        this.graphics.clear();
+      this.angular1.destroy();
+
     }
+    // if (this.input.pointer1.x==)
 
-    this.graphics.fillStyle(0xff0000, 1);
-    this.graphics.fillRect(this.input.pointer1.x, this.input.pointer1.y, 44, 44);
+    // if (this.input.pointer1.isDown || this.input.pointer2.isDown)
+    // {
+    //     this.graphics.clear();
+    // }
 
-    this.graphics.fillStyle(0x00ff00, 1);
-    this.graphics.fillRect(this.input.pointer2.x, this.input.pointer2.y, 44, 44);
+    // this.graphics.fillStyle(0xff0000, 1);
+    // this.graphics.fillRect(this.input.pointer1.x, this.input.pointer1.y, 44, 44);
+
+    // this.graphics.fillStyle(0x00ff00, 1);
+    // this.graphics.fillRect(this.input.pointer2.x, this.input.pointer2.y, 44, 44);
 1
   }
 
