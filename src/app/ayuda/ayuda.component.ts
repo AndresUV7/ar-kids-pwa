502c81255095ecd069d68fa8cffeec790296fe0d
heroe-geek <heroe-geek@users.noreply.github.com>
2021-03-12 21:57:49 -0500

    TERMINADO

--- src/app/ayuda/ayuda.component.ts
+++ src/app/ayuda/ayuda.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ayuda.component.css']
 })
 export class AyudaComponent implements OnInit {
-  pdfSrc = "https://firebasestorage.googleapis.com/v0/b/arkids-da65c.appspot.com/o/MANUAL%20DE%20USUARIO.pdf?alt=media&token=d48cfc36-3bab-43c4-aafc-e7e2f6ed48a0";
+  pdfSrc = "https://firebasestorage.googleapis.com/v0/b/arkids-da65c.appspot.com/o/MANUAL%20DE%20USUARIO.pdf?alt=media&token=c45dcb0d-3880-4496-8e9f-e7c9a1ec6b49";
   constructor() { }
 
   ngOnInit() {
